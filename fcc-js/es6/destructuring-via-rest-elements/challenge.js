a97329d2a63a95d2f740037352bcd2b2cb385b45
Adeleye Olayinka <adeleyeolayinka73@gmail.com>
2023-10-16 08:11:07 +0100

    add first fifteen es6 coding challenge (#4)
    ### This PR adds the first fifteen ES6 coding challenges

--- fcc-js/es6/destructuring-via-rest-elements/challenge.js
+++ fcc-js/es6/destructuring-via-rest-elements/challenge.js
@@ -1,1 +1,11 @@
 // Solve the challenge in this JavaScript file
+
+function removeFirstTwo(list) {
+  const [, , ...asideOneTwo] = list;
+  return asideOneTwo;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+console.log(sourceWithoutFirstTwo);
--- fcc-js/es6/use-destructuring-assignment-to-assign-variables-from-arrays/challenge.js
+++ fcc-js/es6/use-destructuring-assignment-to-assign-variables-from-arrays/challenge.js
@@ -1,1 +1,7 @@
 // Solve the challenge in this JavaScript file
+
+let a = 8;
+let b = 6;
+// Only change code below this line
+
+[a, b] = [b, a];
