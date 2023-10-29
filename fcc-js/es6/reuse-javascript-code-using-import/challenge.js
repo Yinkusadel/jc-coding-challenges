001be2a4a96ee56a3b3e9f65ee55afc830c76106
Adeleye Olayinka <adeleyeolayinka73@gmail.com>
2023-10-29 09:12:19 +0100

    Feat add first 25 es6 coding challenge (#6)

--- fcc-js/es6/reuse-javascript-code-using-import/challenge.js
+++ fcc-js/es6/reuse-javascript-code-using-import/challenge.js
@@ -1,1 +1,6 @@
 // Solve the challenge in this JavaScript file
+import { uppercaseString, lowercaseString } from './string_functions';
+// Only change code above this line
+
+uppercaseString('hello');
+lowercaseString('WORLD!');
--- fcc-js/es6/use-export-to-share-a-code-block/challenge.js
+++ fcc-js/es6/use-export-to-share-a-code-block/challenge.js
@@ -1,1 +1,10 @@
 // Solve the challenge in this JavaScript file
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+};
+
+const lowercaseString = (string) => {
+  return string.toLowerCase();
+};
+
+export { uppercaseString, lowercaseString };
