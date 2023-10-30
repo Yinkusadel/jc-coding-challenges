281efada8d8fe43fa29ff6031436516a814d5a3e
yinkusadel <adeleyeolayinka73@gmail.com>
2023-10-30 20:17:47 +0100

    feat(code): add first 5 js regular expressions coding challenge

--- fcc-js/regular-expressions/ignore-case-while-matching/challenge.js
+++ fcc-js/regular-expressions/ignore-case-while-matching/challenge.js
@@ -1,1 +1,6 @@
 // Solve the challenge in this JavaScript file
+const myString = 'freeCodeCamp';
+const fccRegex = /freeCodeCamp/i;
+const result = fccRegex.test(myString);
+
+console.log(result);
--- fcc-js/regular-expressions/match-literal-strings/challenge.js
+++ fcc-js/regular-expressions/match-literal-strings/challenge.js
@@ -1,1 +1,7 @@
 // Solve the challenge in this JavaScript file
+const waldoIsHiding = 'Somewhere Waldo is hiding in this text.';
+const waldoRegex = /Waldo/;
+waldoRegex.test(waldoIsHiding);
+const result = waldoRegex.test(waldoIsHiding);
+
+console.log(result);
