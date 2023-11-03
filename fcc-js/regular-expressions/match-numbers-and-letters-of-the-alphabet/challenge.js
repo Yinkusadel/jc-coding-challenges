e470816d602228681797d1ee027b0d1974667cbd
yinkusadel <adeleyeolayinka73@gmail.com>
2023-11-03 11:27:13 +0100

    feat(code): add first 10 js regular expressions

--- fcc-js/regular-expressions/match-numbers-and-letters-of-the-alphabet/challenge.js
+++ fcc-js/regular-expressions/match-numbers-and-letters-of-the-alphabet/challenge.js
@@ -1,1 +1,6 @@
 // Solve the challenge in this JavaScript file
+const quoteSample = 'Blueberry 3.141592653s are delicious.';
+const myRegex = /[h-s2-6]/gi; // Change this line
+const result = quoteSample.match(myRegex); // Change this line
+
+console.log(result);
--- fcc-js/regular-expressions/match-single-character-with-multiple-possibilities/challenge.js
+++ fcc-js/regular-expressions/match-single-character-with-multiple-possibilities/challenge.js
@@ -1,1 +1,7 @@
 // Solve the challenge in this JavaScript file
+const quoteSample =
+  'Beware of bugs in the above code; I have only proved it correct, not tried it.';
+const vowelRegex = /[aeiou]/gi; // Change this line
+const result = quoteSample.match(vowelRegex); // Change this line
+
+console.log(result);
