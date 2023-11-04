fccfb7dbf82fee01bc987ae3922f73d91beb5662
Adeleye Olayinka <adeleyeolayinka73@gmail.com>
2023-11-04 13:50:32 +0100

    Feat: add first 10 fcc js regular expressions coding challenge (#9)
    ### This PR adds the first 10 fcc js regular expressions coding
    challenge

--- fcc-js/regular-expressions/match-anything-with-wildcard-period/challenge.js
+++ fcc-js/regular-expressions/match-anything-with-wildcard-period/challenge.js
@@ -1,1 +1,6 @@
 // Solve the challenge in this JavaScript file
+const exampleStr = "Let's have fun with regular expressions!";
+const unRegex = /un./; // Change this line
+const result = unRegex.test(exampleStr);
+
+console.log(result);
--- fcc-js/regular-expressions/match-letters-of-the-alphabet/challenge.js
+++ fcc-js/regular-expressions/match-letters-of-the-alphabet/challenge.js
@@ -1,1 +1,6 @@
 // Solve the challenge in this JavaScript file
+const quoteSample = 'The quick brown fox jumps over the lazy dog.';
+const alphabetRegex = /[a-z]/gi; // Change this line
+const result = quoteSample.match(alphabetRegex); // Change this line
+
+console.log(result);
