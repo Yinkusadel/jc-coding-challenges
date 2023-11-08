bd34085126f7fd9d4448b6390d8745b4cebb4e23
yinkusadel <adeleyeolayinka73@gmail.com>
2023-11-08 12:31:27 +0100

    feat(code): add first 20 js regular expressions

--- fcc-js/regular-expressions/match-all-letters-and-numbers/challenge.js
+++ fcc-js/regular-expressions/match-all-letters-and-numbers/challenge.js
@@ -1,1 +1,6 @@
 // Solve the challenge in this JavaScript file
+const quoteSample = 'The five boxing wizards jump quickly.';
+const alphabetRegexV2 = /\w/g; // Change this line
+const result = quoteSample.match(alphabetRegexV2).length;
+
+console.log(result);
--- fcc-js/regular-expressions/match-beginning-string-patterns/challenge.js
+++ fcc-js/regular-expressions/match-beginning-string-patterns/challenge.js
@@ -1,1 +1,6 @@
 // Solve the challenge in this JavaScript file
+const rickyAndCal = 'Cal and Ricky both like racing.';
+const calRegex = /^Cal/; // Change this line
+const result = calRegex.test(rickyAndCal);
+
+console.log(result);
