d861f14bdd99d79c133c54eccc0a7036a2817b7e
Adeleye Olayinka <adeleyeolayinka73@gmail.com>
2023-11-08 11:58:33 +0100

    Feat: add first 15 fcc js regular expression coding challenge (#10)
    ### This PR adds the first 15 fcc js regular expression coding challenge

--- fcc-js/regular-expressions/find-characters-with-lazy-matching/challenge.js
+++ fcc-js/regular-expressions/find-characters-with-lazy-matching/challenge.js
@@ -1,1 +1,6 @@
 // Solve the challenge in this JavaScript file
+const text = '<h1>Winter is coming</h1>';
+const myRegex = /<.*?>/; // Change this line
+const result = text.match(myRegex);
+
+console.log(result);
