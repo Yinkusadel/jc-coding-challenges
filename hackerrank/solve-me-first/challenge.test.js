eb47515535f0265b6d1ab6cc658f5cea607bd86c
Adeleye Olayinka <adeleyeolayinka73@gmail.com>
2023-11-16 09:57:22 +0100

    feat: add solve me first hackerrank coding challenge (#12)
    ### This PR solves the challenge by completing the function solveMeFirst
    to compute the sum of two integers.

---
+++ hackerrank/solve-me-first/challenge.test.js
@@ -0,0 +1,9 @@
+const solveMeFirst = require('./challenge');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(solveMeFirst(1, 2)).toBe(3);
+});
+
+test('adds 1000 + 100 to equal 1100', () => {
+  expect(solveMeFirst(1000, 100)).toBe(1100);
+});
