7009afd11b16ffb59ebc3067f4d3e6b381c66b1b
yinkusadel <adeleyeolayinka73@gmail.com>
2023-11-16 20:41:47 +0100

    refactor: simple array sum

--- hackerrank/simple-array-sum/challenge.js
+++ hackerrank/simple-array-sum/challenge.js
@@ -1,24 +1,12 @@
 // Solve the challenge in this JavaScript file
 
 const simpleArraySum = (ar) => {
-  let sum = ar;
-  sum = 1 + 2 + 3 + 4 + 10 + 11;
+  // Write your code here
+  let sum = 0;
+  for (let i = 0; i < ar.length; i + 1) {
+    sum += ar[i];
+  }
   return sum;
 };
-
-// const simpleArraySum = (ar) => {
-//   // Write your code here
-//   let sum = 0;
-//   for (let i = 0; i < ar.length; i += 1) {
-//     sum += ar[i];
-//   }
-
-//   return sum;
-// };
-
-// const simpleArraySum = (ar) => {
-//   // Write your code here
-//   return 31;
-// };
 
 module.exports = simpleArraySum;
