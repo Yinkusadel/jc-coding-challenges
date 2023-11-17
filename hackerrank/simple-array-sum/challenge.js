43d797a183df04a0af35696b58a30b84f3535f14
yinkusadel <adeleyeolayinka73@gmail.com>
2023-11-17 14:53:27 +0100

    refactor: simple array sum to add extra test

--- hackerrank/simple-array-sum/challenge.js
+++ hackerrank/simple-array-sum/challenge.js
@@ -1,11 +1,12 @@
 // Solve the challenge in this JavaScript file
 
 const simpleArraySum = (ar) => {
-  // Write your code here
   let sum = 0;
-  for (let i = 0; i < ar.length; i += 1) {
-    sum += ar[i];
-  }
+
+  ar.forEach((element) => {
+    sum += element;
+  });
+
   return sum;
 };
 
