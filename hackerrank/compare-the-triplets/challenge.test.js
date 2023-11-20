9b39a09a896e8924dca6084cfa8322436198ff74
yinkusadel <adeleyeolayinka73@gmail.com>
2023-11-20 15:33:11 +0100

    feat: add test for compare the triplets hackerrank

--- hackerrank/compare-the-triplets/challenge.test.js
+++ hackerrank/compare-the-triplets/challenge.test.js
@@ -1,3 +1,18 @@
 const compareTriplets = require('./challenge');
 
-console.log(compareTriplets);
+describe('compareTriplets', () => {
+  it('should return [0, 0] for input arrays [1, 2, 3] and [1, 2, 3]', () => {
+    const result = compareTriplets([1, 2, 3], [1, 2, 3]);
+    expect(result).toEqual([0, 0]);
+  });
+
+  it('should return [1, 1] for input arrays [5, 6, 7] and [3, 6, 10]', () => {
+    const result = compareTriplets([5, 6, 7], [3, 6, 10]);
+    expect(result).toEqual([1, 1]);
+  });
+
+  it('should return [3, 0] for input arrays [5, 6, 7] and [3, 6, 10]', () => {
+    const result = compareTriplets([10, 15, 20], [5, 6, 7]);
+    expect(result).toEqual([3, 0]);
+  });
+});
