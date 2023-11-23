8015d557bbc47d1e2ce3eec29308760500c34df7
yinkusadel <adeleyeolayinka73@gmail.com>
2023-11-23 21:39:45 +0100

    refactor: a very big sum to use array.reduce

--- hackerrank/a-very-big-sum/challenge.js
+++ hackerrank/a-very-big-sum/challenge.js
@@ -1,13 +1,7 @@
 // Solve the challenge in this JavaScript file
 
 const aVeryBigSum = (ar) => {
-  let sum = 0;
-
-  ar.forEach((element) => {
-    sum += element;
-  });
-
-  return sum;
+  return ar.reduce((sum, element) => sum + element, 0);
 };
 
 module.exports = aVeryBigSum;
